53dfd3ba2faae528527ecf4a5d3067f7430f4166
Garvit Sharma <garvitsh003@gmail.com>
2023-10-01 18:08:28 +0530

    Update pramaanpage.component.ts

--- frontendqr/src/app/pramaanpage/pramaanpage.component.ts
+++ frontendqr/src/app/pramaanpage/pramaanpage.component.ts
@@ -18,6 +18,6 @@ export class PramaanpageComponent {
         this.router.navigate(['/register']);
       }
     verification() {
-        this.router.navigate(['/user-verification']);
+        this.router.navigate(['/generate-qr']);
       }
 }  
